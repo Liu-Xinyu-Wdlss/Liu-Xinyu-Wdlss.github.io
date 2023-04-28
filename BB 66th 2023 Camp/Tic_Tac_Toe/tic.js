4b11397d2575fe9a24539d4f01e5320283a541c3
Liu-Xinyu-Wdlss <125467377+Liu-Xinyu-Wdlss@users.noreply.github.com>
2023-04-28 18:03:28 +0800

    Fixing Bugs :skull:

--- BB 66th 2023 Camp/Tic_Tac_Toe/tic.js
+++ BB 66th 2023 Camp/Tic_Tac_Toe/tic.js
@@ -251,10 +251,10 @@ function myfunc_3() {
 	if (flag == 1) {
 		document.getElementById("b1").value = "X";
 		document.getElementById("b1").disabled = true;
-		flag = O;
+		flag = 0;
 	}
 	else {
-		document.getElementById("b1").value = "O";
+		document.getElementById("b1").value = "0";
 		document.getElementById("b1").disabled = true;
 		flag = 1;
 	}
@@ -264,10 +264,10 @@ function myfunc_4() {
 	if (flag == 1) {
 		document.getElementById("b2").value = "X";
 		document.getElementById("b2").disabled = true;
-		flag = O;
+		flag = 0;
 	}
 	else {
-		document.getElementById("b2").value = "O";
+		document.getElementById("b2").value = "0";
 		document.getElementById("b2").disabled = true;
 		flag = 1;
 	}
@@ -277,10 +277,10 @@ function myfunc_5() {
 	if (flag == 1) {
 		document.getElementById("b3").value = "X";
 		document.getElementById("b3").disabled = true;
-		flag = O;
+		flag = 0;
 	}
 	else {
-		document.getElementById("b3").value = "O";
+		document.getElementById("b3").value = "0";
 		document.getElementById("b3").disabled = true;
 		flag = 1;
 	}
@@ -290,10 +290,10 @@ function myfunc_6() {
 	if (flag == 1) {
 		document.getElementById("b4").value = "X";
 		document.getElementById("b4").disabled = true;
-		flag = O;
+		flag = 0;
 	}
 	else {
-		document.getElementById("b4").value = "O";
+		document.getElementById("b4").value = "0";
 		document.getElementById("b4").disabled = true;
 		flag = 1;
 	}
@@ -303,10 +303,10 @@ function myfunc_7() {
 	if (flag == 1) {
 		document.getElementById("b5").value = "X";
 		document.getElementById("b5").disabled = true;
-		flag = O;
+		flag = 0;
 	}
 	else {
-		document.getElementById("b5").value = "O";
+		document.getElementById("b5").value = "0";
 		document.getElementById("b5").disabled = true;
 		flag = 1;
 	}
@@ -316,10 +316,10 @@ function myfunc_8() {
 	if (flag == 1) {
 		document.getElementById("b6").value = "X";
 		document.getElementById("b6").disabled = true;
-		flag = O;
+		flag = 0;
 	}
 	else {
-		document.getElementById("b6").value = "O";
+		document.getElementById("b6").value = "0";
 		document.getElementById("b6").disabled = true;
 		flag = 1;
 	}
@@ -329,23 +329,23 @@ function myfunc_9() {
 	if (flag == 1) {
 		document.getElementById("b7").value = "X";
 		document.getElementById("b7").disabled = true;
-		flag = O;
+		flag = 0;
 	}
 	else {
-		document.getElementById("b7").value = "O";
+		document.getElementById("b7").value = "0";
 		document.getElementById("b7").disabled = true;
 		flag = 1;
 	}
 }
 
-function myfunc_1O() {
+function myfunc_10() {
 	if (flag == 1) {
 		document.getElementById("b8").value = "X";
 		document.getElementById("b8").disabled = true;
-		flag = O;
+		flag = 0;
 	}
 	else {
-		document.getElementById("b8").value = "O";
+		document.getElementById("b8").value = "0";
 		document.getElementById("b8").disabled = true;
 		flag = 1;
 	}
@@ -355,10 +355,10 @@ function myfunc_11() {
 	if (flag == 1) {
 		document.getElementById("b9").value = "X";
 		document.getElementById("b9").disabled = true;
-		flag = O;
+		flag = 0;
 	}
 	else {
-		document.getElementById("b9").value = "O";
+		document.getElementById("b9").value = "0";
 		document.getElementById("b9").disabled = true;
 		flag = 1;
 	}
